4bd7d2263a3a18be5840d056370c3e3d05a00f41
Kinkela <gedeon.kinkela@kadea.local>
2023-08-15 12:34:00 +0100

    calcule des arri

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sélection des éléments
+//sélection des élément
 const inputField = document.getElementById("input");
 const calculationLabel = document.getElementById("calcul");
 const clearButton = document.getElementById("clear");
@@ -19,52 +19,66 @@ for (let i = 0; i < digitButtons.length; i++) {
     addToInput(this.innerText);
   });
 }
+
 // Ajout d'un écouteur d'événement pour le bouton dot
-dotButton[0].addEventListener("click", function() {
+dotButton[0].addEventListener("click", function(a) {
   addDotToInput();
+  a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton clear
 clearButton.addEventListener("click", function() {
   clearInput();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton plus/minus
-plusMinusButton.addEventListener("click", function() {
+plusMinusButton.addEventListener("click", function(a) {
   changeSign();
+  a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton pourcentage
-percentageButton.addEventListener("click", function() {
+percentageButton.addEventListener("click", function(a) {
   calculatePercentage();
+  a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton divide
 divideButton.addEventListener("click", function(a) {
   addOperatorToInput("÷");
   a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton multiply
 multiplyButton.addEventListener("click", function(a) {
   addOperatorToInput("×");
   a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton minus
 minusButton.addEventListener("click", function(a) {
   addOperatorToInput("-");
   a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton plus
 plusButton.addEventListener("click", function(a) {
   addOperatorToInput("+");
   a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton equal
 equalsButton.addEventListener("click", function(a) {
   performCalculation();
   a.preventDefault();
 });
+
 // Ajout d'un écouteur d'événement pour le bouton reset
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", function(a) {
   resetCalculator();
+  a.preventDefault();
 });
-// Fonction pour ajouter un chiffre à l'entrée
+
 function addToInput(digit) {
   inputField.value += digit;
 }
@@ -79,7 +93,7 @@ function addDotToInput() {
 function clearInput() {
   inputField.value = "";
 }
-// Fonction pour changer le signe de l'entrée
+
 function changeSign() {
   if (inputField.value === "") {
     return;
@@ -93,7 +107,7 @@ function calculatePercentage() {
   }
   inputField.value = parseFloat(inputField.value) / 100;
 }
-// Fonction pour ajouter un opérateur à l'entrée
+
 function addOperatorToInput(operator) {
   if (inputField.value === "") {
     return;
@@ -105,8 +119,31 @@ function performCalculation() {
   if (inputField.value === "") {
     return;
   }
-  calculationLabel.innerText = inputField.value + " =";
-  inputField.value = eval(inputField.value);
+  
+  const expression = inputField.value;
+  
+  if (isValidExpression(expression)) {
+    calculationLabel.innerText = expression + " =";
+    
+    try {
+      const result = evaluateExpression(expression);
+      inputField.value = result;
+    } catch (error) {
+      console.log("Erreur lors de l'évaluation de l'expression :", error);
+    }
+  } else {
+    console.log("Expression invalide !");
+  }
+}
+
+function isValidExpression(expression) {
+  const regex = /^[\d+\-×÷.()\s]+$/;
+  return regex.test(expression);
+}
+
+function evaluateExpression(expression) {
+  const sanitizedExpression = expression.replace(/×/g, "*").replace(/÷/g, "/");
+  return eval(sanitizedExpression);
 }
 // Fonction pour réinitialiser la calculatrice
 function resetCalculator() {
